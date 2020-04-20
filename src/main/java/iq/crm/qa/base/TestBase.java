a6a738632981626eeeb99950d362b03bf4a03bc3
RAMESH <ramesh@creativemoments.lk>
2020-04-20 17:45:57 +0530

    added new comments

--- src/main/java/iq/crm/qa/base/TestBase.java
+++ src/main/java/iq/crm/qa/base/TestBase.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.support.events.EventFiringWebDriver;
 import iq.crm.qa.util.TestUtil;
 import iq.crm.qa.util.WebEventListener;
 
+//base class methods
 public class TestBase {
 	
 	public static WebDriver driver;
